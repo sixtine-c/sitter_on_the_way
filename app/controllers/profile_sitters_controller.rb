bfd1e0043a184aa0c4b72cee87e71b2cedfa0064
Sixtine Caspar <sixcaspar@gmail.com>
2021-11-15 17:34:50 +0100

    controllers generation,routes & has_many in models

---
+++ app/controllers/profile_sitters_controller.rb
@@ -0,0 +1,2 @@
+class ProfileSittersController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :profile_sitters do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index]
+
+  get '/booking/:id/reply', to: 'bookings#reply'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
