e59061675d3154d008e90225bcfa8f6f65dbd2c3
Brice ADJIVON <badjivon@fastmail.com>
2021-11-16 16:26:41 +0100

    Réparation bdd + controller profils, conflit à prévoir avec Sixtine sur les premières lignes (sa version est ok)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,6 @@
 class BookingsController < ApplicationController
 
   def index
-    @booking = Booking.all
     @bookings = policy_scope(Booking).order(created_at: :desc)
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_160403) do
+ActiveRecord::Schema.define(version: 2021_11_16_141345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,13 +29,12 @@ ActiveRecord::Schema.define(version: 2021_11_15_160403) do
 
   create_table "profile_sitters", force: :cascade do |t|
     t.text "description"
-    t.string "addess"
     t.date "start_date"
     t.date "end_date"
     t.bigint "user_id", null: false
-    t.string "dates"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
     t.index ["user_id"], name: "index_profile_sitters_on_user_id"
   end
 
