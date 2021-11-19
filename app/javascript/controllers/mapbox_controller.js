3d42ee66e26a843add93635bf0068aa2c0d6d6b8
nb <nb.bischoff@gmail.com>
2021-11-19 14:40:23 +0100

    searchbar

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -33,6 +33,6 @@ export default class extends Controller {
   _fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
     this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2000 });
   }
 }
